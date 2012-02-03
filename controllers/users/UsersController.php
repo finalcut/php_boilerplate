40c6b983ea1f6c68ae1aef189a39719794050dc5
Bill Rawlinson <rawlins2@marshall.edu>
2012-02-03 17:09:32 -0500

    added demo of passing object from controller to view

--- controllers/users/UsersController.php
+++ controllers/users/UsersController.php
@@ -1,8 +1,18 @@
 <?php
+	require 'model/user.php';
+
 	class UsersController {
 		function home(){
 			F3::set('html_title', 'Become a User');
 			F3::set('content','users/signup.html');
+			
+			$demo = new User();
+			$demo->username="adventure";
+			$demo->firstname="Remo";
+			$demo->lastname="Williams";
+			$demo->role="admin";
+
+			F3::set('user', $demo);
 			echo Template::serve('layout.html');
 		}
 	}
