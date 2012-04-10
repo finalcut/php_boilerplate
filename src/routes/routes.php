37b23473cb00a54322581b143006c4bd2c53ad79
Bill Rawlinson <rawlins2@marshall.edu>
2012-04-10 15:04:21 -0400

    commented out the html_title usage for the / route.  Added some commentary to explain how to set the html_title value and how it is defaulted.

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,7 +1,11 @@
 <?php
 	F3::route('GET /',
 		function() {
-			F3::set('html_title', 'PHP Boilerplate Application');
+			// set the html_title variable if you want a page specific title.. If you don't then the value stored in the projectname configuration variable will be used as the page title.
+			// look in config.cfg to see what the projectname is.   Here is an example of setting the html_title variable:
+			// F3::set('html_title', 'PHP Boilerplate Application');
+			
+
 			F3::set('content','home.html');
 			echo Template::serve('layout/site.html');
 		});
