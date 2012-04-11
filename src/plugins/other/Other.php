d1e2ec9b92eb1ddf0a3e0426a72a0ec16c00027a
Bill Rawlinson <rawlins2@marshall.edu>
2012-04-11 16:41:33 -0400

    added a little more logic to help teach the usage of the framework

--- src/plugins/other/Other.php
+++ src/plugins/other/Other.php
@@ -5,7 +5,8 @@
 
 	class Other extends \marshall\core\BaseController {
 		function foo(){
-			F3::set('html_title', 'Bar');
+			F3::set('html_title', F3::get('PARAMS["thisPage"]'));
+			F3::set('thisPage', F3::get('PARAMS["thisPage"]'));
 			F3::set('content','other/views/other.html');
 			echo Template::serve('core/layout/site.html');
 		}
--- src/plugins/other/_routes.php
+++ src/plugins/other/_routes.php
@@ -4,5 +4,5 @@
 	$plugin = new \php_boilerplate\plugins\other\_plugin();
 
 	// define the plugin routes
-	F3::route('GET /other/*', '\php_boilerplate\plugins\other\Other->foo');
+	F3::route('GET /other/@thisPage', '\php_boilerplate\plugins\other\Other->foo');
 ?>
