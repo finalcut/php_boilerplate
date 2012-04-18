9ba3a03e510fe3f71461a42d00f631e5a8a0871e
Bill Rawlinson <rawlins2@marshall.edu>
2012-04-18 14:06:45 -0400

    some javascript formatting.. more to come most likely.

--- src/plugins/directory/views/js/main.js
+++ src/plugins/directory/views/js/main.js
@@ -1,22 +1,21 @@
 var AppRouter = Backbone.Router.extend({
 
-    /*
+/*
         I am using the * at the beginning of the route becuase I don't know regex any better
         and our AppRouter is located within a subdirectory of the webserver.  Feel free to tweak
         this for your own needs if you can make it more efficient/selective.
     */
-    routes:{
-        "*directory":"home",
+    routes: {
+        "*directory": "home",
     },
 
 
     // this is intentionally left blank.  I don't really have anything to do on this examples initialization.
-    initialize:function () {
-    },
+    initialize: function() {},
 
 
     // used by the routes above; basically when */directory is loaded this function is called.
-    home:function () {
+    home: function() {
         // Since the home view never changes, we instantiate it and render it only once
         if (!this.homeView) {
             this.homeView = new HomeView();
@@ -27,8 +26,7 @@ var AppRouter = Backbone.Router.extend({
 });
 
 // here we will cache our templates; check out util.js for how this is all working.
-tpl.loadTemplates(['home', 'employee-list-item'],
-    function () {
-        app = new AppRouter();
-        Backbone.history.start();
-    });
+tpl.loadTemplates(['home', 'employee-list-item'], function() {
+    app = new AppRouter();
+    Backbone.history.start();
+});
