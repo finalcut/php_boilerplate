711a1fe37077d639c5e879ab590873d0120c12ff
Bill Rawlinson <rawlins2@marshall.edu>
2012-09-27 17:03:26 -0400

    improved handling of failed login

--- src/plugins/ldap/views/login.js
+++ src/plugins/ldap/views/login.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
 		jQuery.post($("#loginForm").attr("action"), {username: $("#txtUsername").val(), password: $("#txtPassword").val()}, function(data, textStatus, xhr) {
 			data = jQuery.parseJSON(data);
 			if(data.status ==1){
+				$("#loginerror").html("");
 				window.location= $("#returnTo").val();
+			} else {
+				$("#loginerror").html(data.message);
+				$("#loginerror").removeClass("hidden");
+				$("#loginerror").fadeOut(2000);
 			}
 		});
 
